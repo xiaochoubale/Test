3b09267041e96db78ad82058e1f4c274dcf187a9
ChengKang <2467907002@qq.com>
2023-10-23 15:12:02 +0800

    Initial Commit 3

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TestTheme {
-        Greeting("Android")
+        Greeting("Android2")
     }
 }
